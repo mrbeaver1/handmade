857cc63767edd8d0abcdc93427f8428f8ef87503
mrbeaver1 <zpierunov@mail.ru>
2021-01-28 02:44:00 +0300

    Создал сущность юзера

---
+++ src/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="`user`")
+ */
+class User implements UserInterface
+{
+    public function getRoles()
+    {
+        // TODO: Implement getRoles() method.
+    }
+
+    public function getPassword()
+    {
+        // TODO: Implement getPassword() method.
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function getUsername()
+    {
+        // TODO: Implement getUsername() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
